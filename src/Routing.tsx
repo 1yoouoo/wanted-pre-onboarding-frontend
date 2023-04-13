ff0d7833ade20691ba5a6552e2b621d34adb8166
1yoouoo <1yoouoo@gmail.com>
2023-04-14 00:15:29 +0900

    feat: Update Routing for redirect

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SignUpPage from './Pages/SignUpPage';
 import SignInPage from './Pages/SignInPage';
+import ToDoPage from './Pages/ToDoPage';
+import { isLogin } from './utills/function';
 
-const Routing: React.FC = (): JSX.Element => (
-  <BrowserRouter>
-    <Routes>
-      <Route path='/signup' element={<SignUpPage />} />
-      <Route path='/signin' element={<SignInPage />} />
-    </Routes>
-  </BrowserRouter>
-);
+const Routing: React.FC = (): JSX.Element => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signup' element={isLogin() ? <Navigate to='/todo' /> : <SignUpPage />} />
+        <Route path='/signin' element={isLogin() ? <Navigate to='/todo' /> : <SignInPage />} />
+        <Route path='/todo' element={isLogin() ? <ToDoPage /> : <Navigate to='/signin' />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default Routing;
