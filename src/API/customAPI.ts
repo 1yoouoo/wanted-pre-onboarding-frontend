832a08d9e7e8d45aafaadf7f8cddd6cdf71ad3dd
1yoouoo <1yoouoo@gmail.com>
2023-04-13 23:18:57 +0900

    feat: Create customAPI.ts

---
+++ src/API/customAPI.ts
@@ -0,0 +1,42 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from 'axios';
+
+const ApiBase = 'https://www.pre-onboarding-selection-task.shop/';
+
+const axiosApi = ({ options }: any) => {
+  const instance = axios.create({
+    baseURL: ApiBase,
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  instance.interceptors.response.use(
+    (response) => {
+      console.log('interceptor > response', response);
+      return response;
+    },
+    (error) => {
+      console.log('interceptor > error', error);
+      alert(error.response.data.message);
+      return Promise.reject(error);
+    }
+  );
+  instance.interceptors.request.use(
+    (request) => {
+      console.log('interceptor > request', request);
+      return request;
+    },
+    (error) => {
+      console.log('interceptor > error', error);
+      alert(error.response.data.message);
+      return Promise.reject(error);
+    }
+  );
+  instance.defaults.timeout = 2500;
+  return instance;
+};
+
+export const defaultInstance = axiosApi(ApiBase);
