45a6d4458e0f630d3f0fbc3848fe99e3be6f7da9
1yoouoo <1yoouoo@gmail.com>
2023-04-14 17:26:42 +0900

    feat: Create getTodos func & Add axiosAuthApi

--- src/API/customAPI.ts
+++ src/API/customAPI.ts
@@ -4,6 +4,7 @@
 import axios from 'axios';
 
 const ApiBase = 'https://www.pre-onboarding-selection-task.shop/';
+const token = localStorage.getItem('token');
 
 const axiosApi = ({ options }: any) => {
   const instance = axios.create({
@@ -39,4 +40,39 @@ const axiosApi = ({ options }: any) => {
   return instance;
 };
 
+const axiosAuthApi = ({ options }: any) => {
+  const instance = axios.create({
+    baseURL: ApiBase,
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  instance.interceptors.response.use(
+    (response) => {
+      console.log('interceptor > response', response);
+      return response;
+    },
+    (error) => {
+      console.log('interceptor > error', error);
+      return Promise.reject(error);
+    }
+  );
+  instance.interceptors.request.use(
+    (request) => {
+      console.log('interceptor > request', request);
+
+      return request;
+    },
+    (error) => {
+      console.log('interceptor > error', error);
+      return Promise.reject(error);
+    }
+  );
+  instance.defaults.timeout = 2500;
+  return instance;
+};
+
 export const defaultInstance = axiosApi(ApiBase);
+export const authInstance = axiosAuthApi(ApiBase);
